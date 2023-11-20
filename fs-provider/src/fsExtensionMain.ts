05e8181c425ef96f42fed426608fbf6e0c9f857c
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-20 18:15:13 +0200

    squash!

--- fs-provider/src/fsExtensionMain.ts
+++ fs-provider/src/fsExtensionMain.ts
@@ -3,28 +3,52 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { xhr } from 'request-light';
-import { Uri, FileStat, FileType, workspace, ExtensionContext, FileSystemError } from 'vscode';
-import { Entry, MemFileSystemProvider, File, Directory } from './fsProvider';
+import { xhr } from "request-light";
+import {
+	Uri,
+	FileStat,
+	FileType,
+	workspace,
+	ExtensionContext,
+	FileSystemError,
+} from "vscode";
+import { Entry, MemFileSystemProvider, File, Directory } from "./fsProvider";
 
-const SCHEME = 'vscode-test-web';
+const SCHEME = "vscode-test-web";
 
 export function activate(context: ExtensionContext) {
-	const serverUri = context.extensionUri.with({ path: '/static/mount', query: undefined });
-	const serverBackedRootDirectory = new ServerBackedDirectory(serverUri, [], '');
+	const serverUri = context.extensionUri.with({
+		path: "/static/mount",
+		query: undefined,
+	});
+	const serverBackedRootDirectory = new ServerBackedDirectory(
+		serverUri,
+		[],
+		""
+	);
 
-	const disposable = workspace.registerFileSystemProvider(SCHEME, new MemFileSystemProvider(SCHEME, serverBackedRootDirectory));
+	const disposable = workspace.registerFileSystemProvider(
+		SCHEME,
+		new MemFileSystemProvider(SCHEME, serverBackedRootDirectory)
+	);
 	context.subscriptions.push(disposable);
 
-	console.log(`vscode-test-web-support fs provider registers for ${SCHEME}, initial content from ${serverUri.toString(/*skipEncoding*/ true)}`);
+	console.log(
+		`vscode-test-web-support fs provider registers for ${SCHEME}, initial content from ${serverUri.toString(
+			/*skipEncoding*/ true
+		)}`
+	);
 }
 
 class ServerBackedFile implements File {
 	readonly type = FileType.File;
 	private _stats: Promise<FileStat> | undefined;
 	private _content: Promise<Uint8Array> | undefined;
-	constructor(private readonly _serverRoot: Uri, public pathSegments: readonly string[], public name: string) {
-	}
+	constructor(
+		private readonly _serverRoot: Uri,
+		public pathSegments: readonly string[],
+		public name: string
+	) {}
 	get stats(): Promise<FileStat> {
 		if (this._stats === undefined) {
 			this._stats = getStats(this._serverRoot, this.pathSegments);
@@ -49,8 +73,11 @@ class ServerBackedDirectory implements Directory {
 	readonly type = FileType.Directory;
 	private _stats: Promise<FileStat> | undefined;
 	private _entries: Promise<Map<string, Entry>> | undefined;
-	constructor(private readonly _serverRoot: Uri, public pathSegments: readonly string[], public name: string) {
-	}
+	constructor(
+		private readonly _serverRoot: Uri,
+		public pathSegments: readonly string[],
+		public name: string
+	) {}
 	get stats(): Promise<FileStat> {
 		if (this._stats === undefined) {
 			this._stats = getStats(this._serverRoot, this.pathSegments);
@@ -73,20 +100,36 @@ class ServerBackedDirectory implements Directory {
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function isEntry(e: any): e is Entry {
-	return e && (e.type === FileType.Directory || e.type === FileType.File) && typeof e.name === 'string' && e.name.length > 0;
+	return (
+		e &&
+		(e.type === FileType.Directory || e.type === FileType.File) &&
+		typeof e.name === "string" &&
+		e.name.length > 0
+	);
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function isStat(e: any): e is FileStat {
-	return e && (e.type === FileType.Directory || e.type === FileType.File) && typeof e.ctime === 'number' && typeof e.mtime === 'number' && typeof e.size === 'number';
+	return (
+		e &&
+		(e.type === FileType.Directory || e.type === FileType.File) &&
+		typeof e.ctime === "number" &&
+		typeof e.mtime === "number" &&
+		typeof e.size === "number"
+	);
 }
 
 function getServerUri(serverRoot: Uri, pathSegments: readonly string[]): Uri {
 	return Uri.joinPath(serverRoot, ...pathSegments);
 }
 
-async function getEntries(serverRoot: Uri, pathSegments: readonly string[]): Promise<Map<string, Entry>> {
-	const url = getServerUri(serverRoot, pathSegments).with({ query: 'readdir' }).toString(/*skipEncoding*/ true);
+async function getEntries(
+	serverRoot: Uri,
+	pathSegments: readonly string[]
+): Promise<Map<string, Entry>> {
+	const url = getServerUri(serverRoot, pathSegments)
+		.with({ query: "readdir" })
+		.toString(/*skipEncoding*/ true);
 	const response = await xhr({ url });
 	if (response.status === 200 && response.status <= 204) {
 		try {
@@ -95,8 +138,22 @@ async function getEntries(serverRoot: Uri, pathSegments: readonly string[]): Pro
 				const entries = new Map();
 				for (const r of res) {
 					if (isEntry(r)) {
-						const newPathSegments = [...pathSegments, encodeURIComponent(r.name)];
-						const newEntry: Entry = r.type === FileType.Directory ? new ServerBackedDirectory(serverRoot, newPathSegments, r.name) : new ServerBackedFile(serverRoot, newPathSegments, r.name);
+						const newPathSegments = [
+							...pathSegments,
+							encodeURIComponent(r.name),
+						];
+						const newEntry: Entry =
+							r.type === FileType.Directory
+								? new ServerBackedDirectory(
+										serverRoot,
+										newPathSegments,
+										r.name
+								  )
+								: new ServerBackedFile(
+										serverRoot,
+										newPathSegments,
+										r.name
+								  );
 						entries.set(newEntry.name, newEntry);
 					}
 				}
@@ -107,30 +164,54 @@ async function getEntries(serverRoot: Uri, pathSegments: readonly string[]): Pro
 		}
 		console.log(`Invalid server response format for ${url}.`);
 	} else {
-		console.log(`Invalid server response for ${url}. Status ${response.status}`);
+		console.log(
+			`Invalid server response for ${url}. Status ${response.status}`
+		);
 	}
 	return new Map();
 }
 
-async function getStats(serverRoot: Uri, pathSegments: readonly string[]): Promise<FileStat> {
+async function getStats(
+	serverRoot: Uri,
+	pathSegments: readonly string[]
+): Promise<FileStat> {
 	const serverUri = getServerUri(serverRoot, pathSegments);
-	const url = serverUri.with({ query: 'stat' }).toString(/*skipEncoding*/ true);
+	const url = serverUri
+		.with({ query: "stat" })
+		.toString(/*skipEncoding*/ true);
 	const response = await xhr({ url });
 	if (response.status === 200 && response.status <= 204) {
 		const res = JSON.parse(response.responseText);
 		if (isStat(res)) {
 			return res;
 		}
-		throw FileSystemError.FileNotFound(`Invalid server response for ${serverUri.toString(/*skipEncoding*/ true)}.`);
+		throw FileSystemError.FileNotFound(
+			`Invalid server response for ${serverUri.toString(
+				/*skipEncoding*/ true
+			)}.`
+		);
 	}
-	throw FileSystemError.FileNotFound(`Invalid server response for ${serverUri.toString(/*skipEncoding*/ true)}. Status ${response.status}.`);
+	throw FileSystemError.FileNotFound(
+		`Invalid server response for ${serverUri.toString(
+			/*skipEncoding*/ true
+		)}. Status ${response.status}.`
+	);
 }
 
-async function getContent(serverRoot: Uri, pathSegments: readonly string[]): Promise<Uint8Array> {
+async function getContent(
+	serverRoot: Uri,
+	pathSegments: readonly string[]
+): Promise<Uint8Array> {
 	const serverUri = getServerUri(serverRoot, pathSegments);
-	const response = await xhr({ url: serverUri.toString(/*skipEncoding*/ true) });
+	const response = await xhr({
+		url: serverUri.toString(/*skipEncoding*/ true),
+	});
 	if (response.status >= 200 && response.status <= 204) {
 		return response.body;
 	}
-	throw FileSystemError.FileNotFound(`Invalid server response for ${serverUri.toString(/*skipEncoding*/ true)}. Status ${response.status}.`);
+	throw FileSystemError.FileNotFound(
+		`Invalid server response for ${serverUri.toString(
+			/*skipEncoding*/ true
+		)}. Status ${response.status}.`
+	);
 }
--- src/server/app.ts
+++ src/server/app.ts
@@ -3,34 +3,43 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { ReadStream } from 'fs';
-import * as Koa from 'koa';
-import * as morgan from 'koa-morgan';
-import * as kstatic from 'koa-static';
-import * as kmount from 'koa-mount';
-import * as cors from '@koa/cors';
-import { basename, join } from 'path';
-import { IConfig } from './main';
-import workbench from './workbench';
-import { configureMounts } from './mounts';
-import { prebuiltExtensionsLocation } from './extensions';
+import { ReadStream } from "fs";
+import * as Koa from "koa";
+import * as morgan from "koa-morgan";
+import * as kstatic from "koa-static";
+import * as kmount from "koa-mount";
+import * as cors from "@koa/cors";
+import { basename, join } from "path";
+import { IConfig } from "./main";
+import workbench from "./workbench";
+import { configureMounts } from "./mounts";
+import { prebuiltExtensionsLocation } from "./extensions";
 
 export default async function createApp(config: IConfig): Promise<Koa> {
 	const app = new Koa();
 
-	app.use(morgan('dev', { skip: (req, res) => !config.printServerLog && (res.statusCode >= 200 && res.statusCode < 300) }));
+	app.use(
+		morgan("dev", {
+			skip: (req, res) =>
+				!config.printServerLog &&
+				res.statusCode >= 200 &&
+				res.statusCode < 300,
+		})
+	);
 
 	// CORS
 	app.use(
 		cors({
-			allowMethods: ['GET'],
+			allowMethods: ["GET"],
 			credentials: true,
 			origin: (ctx: Koa.Context) => {
-				const origin = ctx.get('Origin');
+				const origin = ctx.get("Origin");
 				if (
 					/^https:\/\/[^.]+\.vscode-cdn\.net$/.test(origin) || // needed for the webviewContent
 					/^https:\/\/[^.]+\.vscode-webview\.net$/.test(origin) ||
-					new RegExp(`^${ctx.protocol}://[^.]+\\.${ctx.host}$`).test(origin) // match subdomains of localhost
+					new RegExp(`^${ctx.protocol}://[^.]+\\.${ctx.host}$`).test(
+						origin
+					) // match subdomains of localhost
 				) {
 					return origin;
 				}
@@ -40,10 +49,10 @@ export default async function createApp(config: IConfig): Promise<Koa> {
 		})
 	);
 
-	if (config.build.type !== 'sources' && config.build.type !== 'static') {
+	if (config.build.type !== "sources" && config.build.type !== "static") {
 		// CSP: frame-ancestors
 		app.use((ctx, next) => {
-			ctx.set('Content-Security-Policy', `frame-ancestors 'none'`);
+			ctx.set("Content-Security-Policy", `frame-ancestors 'none'`);
 			return next();
 		});
 	}
@@ -51,59 +60,105 @@ export default async function createApp(config: IConfig): Promise<Koa> {
 	// COI
 	app.use((ctx, next) => {
 		// set COOP/COEP depending on vscode-coi-flags
-		const value = ctx.query['vscode-coi'];
-		if (value === '1') {
-			ctx.set('Cross-Origin-Opener-Policy', 'same-origin');
-		} else if (value === '2') {
-			ctx.set('Cross-Origin-Embedder-Policy', 'require-corp');
-		} else if (value === '3' || value === '') {
-			ctx.set('Cross-Origin-Opener-Policy', 'same-origin');
-			ctx.set('Cross-Origin-Embedder-Policy', 'require-corp');
+		const value = ctx.query["vscode-coi"];
+		if (value === "1") {
+			ctx.set("Cross-Origin-Opener-Policy", "same-origin");
+		} else if (value === "2") {
+			ctx.set("Cross-Origin-Embedder-Policy", "require-corp");
+		} else if (value === "3" || value === "") {
+			ctx.set("Cross-Origin-Opener-Policy", "same-origin");
+			ctx.set("Cross-Origin-Embedder-Policy", "require-corp");
 		}
 
 		// set CORP on all resources
-		ctx.set('Cross-Origin-Resource-Policy', 'cross-origin')
-		return next()
-	})
+		ctx.set("Cross-Origin-Resource-Policy", "cross-origin");
+		return next();
+	});
 
 	// shift the line numbers of source maps in extensions by 2 as the content is wrapped by an anonymous function
 	app.use(async (ctx, next) => {
 		await next();
-		if (ctx.status === 200 && ctx.path.match(/\/(dev)?extensions\/.*\.js\.map$/) && ctx.body instanceof ReadStream) {
+		if (
+			ctx.status === 200 &&
+			ctx.path.match(/\/(dev)?extensions\/.*\.js\.map$/) &&
+			ctx.body instanceof ReadStream
+		) {
 			// we know it's a ReadStream as that's what kstatic uses
 			const chunks: Buffer[] = [];
 			for await (const chunk of ctx.body) {
 				chunks.push(Buffer.from(chunk));
 			}
 			const bodyContent = Buffer.concat(chunks).toString("utf-8");
-			ctx.response.body = `{"version":3,"file":"${basename(ctx.path)}","sections":[{"offset":{"line":2,"column":0},"map":${bodyContent} }]}`;
+			ctx.response.body = `{"version":3,"file":"${basename(
+				ctx.path
+			)}","sections":[{"offset":{"line":2,"column":0},"map":${bodyContent} }]}`;
 		}
 	});
 
 	const serveOptions: kstatic.Options = { hidden: true };
 
 	if (config.extensionDevelopmentPath) {
-		console.log('Serving dev extensions from ' + config.extensionDevelopmentPath);
-		app.use(kmount('/static/devextensions', kstatic(config.extensionDevelopmentPath, serveOptions)));
+		console.log(
+			"Serving dev extensions from " + config.extensionDevelopmentPath
+		);
+		app.use(
+			kmount(
+				"/static/devextensions",
+				kstatic(config.extensionDevelopmentPath, serveOptions)
+			)
+		);
 	}
 
-	if (config.build.type === 'static') {
-		app.use(kmount('/static/build', kstatic(config.build.location, serveOptions)));
-	} else if (config.build.type === 'sources') {
-		console.log('Serving VS Code sources from ' + config.build.location);
-		app.use(kmount('/static/sources', kstatic(config.build.location, serveOptions)));
-		app.use(kmount('/static/sources', kstatic(join(config.build.location, 'resources', 'server'), serveOptions))); // for manifest.json, favicon and code icons.
+	if (config.build.type === "static") {
+		app.use(
+			kmount(
+				"/static/build",
+				kstatic(config.build.location, serveOptions)
+			)
+		);
+	} else if (config.build.type === "sources") {
+		console.log("Serving VS Code sources from " + config.build.location);
+		app.use(
+			kmount(
+				"/static/sources",
+				kstatic(config.build.location, serveOptions)
+			)
+		);
+		app.use(
+			kmount(
+				"/static/sources",
+				kstatic(
+					join(config.build.location, "resources", "server"),
+					serveOptions
+				)
+			)
+		); // for manifest.json, favicon and code icons.
 
 		// built-in extension are at 'extensions` as well as prebuilt extensions downloaded from the marketplace
-		app.use(kmount(`/static/sources/extensions`, kstatic(join(config.build.location, prebuiltExtensionsLocation), serveOptions)));
+		app.use(
+			kmount(
+				`/static/sources/extensions`,
+				kstatic(
+					join(config.build.location, prebuiltExtensionsLocation),
+					serveOptions
+				)
+			)
+		);
 	}
 
 	configureMounts(config, app);
 
 	if (config.extensionPaths) {
 		config.extensionPaths.forEach((extensionPath, index) => {
-			console.log('Serving additional built-in extensions from ' + extensionPath);
-			app.use(kmount(`/static/extensions/${index}`, kstatic(extensionPath, serveOptions)));
+			console.log(
+				"Serving additional built-in extensions from " + extensionPath
+			);
+			app.use(
+				kmount(
+					`/static/extensions/${index}`,
+					kstatic(extensionPath, serveOptions)
+				)
+			);
 		});
 	}
 
